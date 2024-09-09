644b588c606a3f2bd509bc6a1c4ab0d8dc70026d
matcop <matcop@gmail.com>
2024-09-09 19:51:53 -0400

    Creando un gradiente de fondo home_page y los views

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:vertical_landig_page/ui/views/about_view.dart';
 import 'package:vertical_landig_page/ui/views/contact_view.dart';
 import 'package:vertical_landig_page/ui/views/home_view.dart';
@@ -11,7 +12,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink,
       body: Container(
+        decoration: buildBoxDecoration(),
         child: Stack(
           children: [
             _HomeBody(),
@@ -21,19 +24,27 @@ class HomePage extends StatelessWidget {
                 child: Container(
                   width: 100,
                   height: 50,
-                  color: Colors.red,
+                  color: Colors.black,
                 ))
           ],
         ),
       ),
     );
   }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+      gradient: LinearGradient(
+          colors: [Colors.pink, Colors.purpleAccent],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.5, 0.5]));
 }
 
 class _HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      physics: BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
       children: [
         HomeView(),
