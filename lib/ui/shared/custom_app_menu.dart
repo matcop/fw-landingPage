f9b3f32ca86b899340f1e22fec1f55ac283a93cb
matcop <matcop@gmail.com>
2024-09-11 10:47:12 -0400

    opciones del menu

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vertical_landig_page/ui/shared/custom_menu_item.dart';
 
-class CustomAppMenu extends StatelessWidget {
+class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({super.key});
+
+  @override
+  State<CustomAppMenu> createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu>
+    with SingleTickerProviderStateMixin {
+  bool isOpen = false;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(microseconds: 200));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,24 +28,75 @@ class CustomAppMenu extends StatelessWidget {
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
         onTap: () {
-          print('click.... gato');
+          if (isOpen) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+
+          setState(() {
+            isOpen = !isOpen;
+          });
         },
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          width: 150,
-          height: 50,
-          color: Colors.black,
-          child: Row(
-            children: [
-              Text(
-                'Menu',
-                style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
-              ),
-              Spacer(),
-              Icon(Icons.menu, color: Colors.white)
-            ],
+            //puede ser AnimatedContainer
+            // duration: Duration(milliseconds: 3000),
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            width: 150,
+            height: isOpen ? 310 : 50,
+            color: Colors.black,
+            child: Column(
+              children: [
+                _MenuTitle(isOpen: isOpen, controller: controller),
+                if (isOpen) ...[
+                  CustomMenuItem(text: 'Home', onPressed: () {}),
+                  CustomMenuItem(text: 'About', onPressed: () {}),
+                  CustomMenuItem(text: 'Pricing', onPressed: () {}),
+                  CustomMenuItem(text: 'Contact', onPressed: () {}),
+                  CustomMenuItem(text: 'Location', onPressed: () {}),
+                  SizedBox(
+                    height: 10,
+                  )
+                ]
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class _MenuTitle extends StatelessWidget {
+  const _MenuTitle({
+    super.key,
+    required this.isOpen,
+    required this.controller,
+  });
+
+  final bool isOpen;
+  final AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 145,
+      height: 50,
+      child: Row(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isOpen ? 45 : 0,
           ),
-        ),
+          Text(
+            'Menu',
+            style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
+          ),
+          Spacer(),
+          AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: controller,
+              color: Colors.white)
+        ],
       ),
     );
   }
---
+++ lib/ui/shared/custom_menu_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomMenuItem extends StatefulWidget {
+  final String text;
+  final Function onPressed;
+
+  const CustomMenuItem(
+      {super.key, required this.text, required this.onPressed});
+
+  @override
+  State<CustomMenuItem> createState() => _CustomMenuItemState();
+}
+
+class _CustomMenuItemState extends State<CustomMenuItem> {
+  bool isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHover = true),
+      onExit: (_) => setState(() => isHover = false),
+      child: GestureDetector(
+        onTap: () => widget.onPressed(),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 700),
+          width: 150,
+          height: 50,
+          color: isHover ? Colors.pinkAccent : Colors.black,
+          child: Center(
+              child: Text(
+            widget.text,
+            style: GoogleFonts.roboto(fontSize: 20, color: Colors.white),
+          )),
+        ),
+      ),
+    );
+  }
+}
