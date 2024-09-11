675267541559681d0c712caf6e2e95b220dbaded
matcop <matcop@gmail.com>
2024-09-11 16:26:02 -0400

    Cambiar el URL al hacer click en un enlace

--- lib/provider/page_provider.dart
+++ lib/provider/page_provider.dart
@@ -1,9 +1,18 @@
+// import 'dart:html' as html;
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
 
+  List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+
   goTo(int index) {
+    // final routeName = _pages[index];
+    // print(routeName);
+//la linea de abajo solo funciona cuando es pagina web.
+    html.window.history.pushState(null, 'none ', '#/${_pages[index]}');
+
     scrollController.animateToPage(index,
         duration: Duration(milliseconds: 3000), curve: Curves.easeInOut);
   }
