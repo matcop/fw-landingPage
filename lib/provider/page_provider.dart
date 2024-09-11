6d37f994ce7cee0144b9e944600be4e7f283c517
matcop <matcop@gmail.com>
2024-09-11 17:45:09 -0400

    Utilizar el segmento para definir la pantalla inicial

--- lib/provider/page_provider.dart
+++ lib/provider/page_provider.dart
@@ -6,8 +6,23 @@ class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
 
   List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+  int _curretIndex = 0;
 
   createScrollController(String routeName) {
+    this.scrollController =
+        new PageController(initialPage: getPageIndex(routeName));
+
+    this.scrollController.addListener(() {
+      final index = (this.scrollController.page ?? 0).round();
+
+      if (index != _curretIndex) {
+        html.window.history.pushState(null, 'none ', '#/${_pages[index]}');
+        _curretIndex = index;
+      }
+    });
+  }
+
+  int getPageIndex(String routeName) {
     return (_pages.indexOf(routeName) == -1) ? 0 : _pages.indexOf(routeName);
   }
 
@@ -15,7 +30,7 @@ class PageProvider extends ChangeNotifier {
     // final routeName = _pages[index];
     // print(routeName);
 //la linea de abajo solo funciona cuando es pagina web.
-    html.window.history.pushState(null, 'none ', '#/${_pages[index]}');
+    // html.window.history.pushState(null, 'none ', '#/${_pages[index]}');
 
     scrollController.animateToPage(index,
         duration: Duration(milliseconds: 3000), curve: Curves.easeInOut);
